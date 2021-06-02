fbd648f05238381db4e66020e27c8ac16cec69d9
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2021-06-02 14:21:27 +0100

    docs: remove now-unused Gulp files and plugins

--- gulp/copy-namespace.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-
-gulp.task('copy-namespace', () => {
-  return gulp.src([
-      'src/moj/namespace.js'
-    ])
-    .pipe(gulp.dest('public/javascripts/'));
-});
--- gulp/server.js
+++
@@ -1,4 +0,0 @@
-const gulp = require('gulp');
-const shell = require('gulp-shell');
-
-gulp.task('server', shell.task('npm run build:docs -- --serve --watch'));
