294466dc6ec075cfcf3f26a77f10fefd6cd6bf5c
Calum Ryan <dev@calumryan.com>
2021-08-26 10:46:35 +0100

    chore: remove unused moj component examples

--- docs/examples/button-menu-collapsible/script.js
+++
@@ -1,7 +0,0 @@
-new MOJFrontend.ButtonMenu({
-  container: $(".moj-button-menu"),
-  mq: "(min-width: 200em)",
-  buttonText: "Menu",
-  buttonClasses:
-    "govuk-button--secondary moj-button-menu__toggle-button--secondary",
-});
