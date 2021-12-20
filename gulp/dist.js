58420e658979e143468cd4e4315900db98c69bc5
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2021-12-20 14:55:45 +0000

    build: create a distribution bundle on release
    Rather than insisting users compile the SCSS and JS themselves, provide a pre-compiled distribution
    version that can be used out-of-the-box
    
    Also stop tagging with the `-alpha` suffix, simplify GitHub tokens for publishing and build docs in
    a Node version that will compile
    
    fixes #228 #282

---
+++ gulp/dist.js
@@ -0,0 +1,48 @@
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const del = require("del");
+const gulp = require("gulp");
+const postcss = require("gulp-postcss");
+const rename = require("gulp-rename");
+const sass = require("gulp-sass")(require("node-sass"));
+const uglify = require("gulp-uglify");
+const zip = require("gulp-zip");
+
+gulp.task("dist:clean", () => {
+  return del(["dist/**/*"]);
+});
+
+gulp.task("dist:assets", () => {
+  return gulp.src("package/moj/assets/**/*").pipe(gulp.dest("dist/assets/"));
+});
+
+gulp.task("dist:javascript", () => {
+  return gulp
+    .src("package/moj/all.js")
+    .pipe(
+      uglify({
+        ie8: true,
+      })
+    )
+    .pipe(rename("moj-frontend.min.js"))
+    .pipe(gulp.dest("dist"));
+});
+
+gulp.task("dist:css", () => {
+  return gulp
+    .src("package/moj/*.scss")
+    .pipe(sass())
+    .pipe(postcss([autoprefixer, cssnano]))
+    .pipe(
+      rename((path) => ({
+        dirname: path.dirname,
+        basename: path.basename.replace("all", "moj-frontend"),
+        extname: ".min.css",
+      }))
+    )
+    .pipe(gulp.dest("dist"));
+});
+
+gulp.task("dist:zip", () => {
+  return gulp.src("dist/**").pipe(zip("release.zip")).pipe(gulp.dest("dist"));
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,27 @@
-const gulp = require('gulp');
-const requireDir = require('require-dir');
+const gulp = require("gulp");
+const requireDir = require("require-dir");
 
-requireDir('./gulp', {
-  recurse: true
+requireDir("./gulp", {
+  recurse: true,
 });
 
-gulp.task('build:package', gulp.series(
-  'build:clean',
-  'build:copy-files',
-  'build:javascript',
-  'build:compress-images'
-));
+gulp.task(
+  "build:package",
+  gulp.series(
+    "build:clean",
+    "build:copy-files",
+    "build:javascript",
+    "build:compress-images"
+  )
+);
+
+gulp.task(
+  "build:dist",
+  gulp.series(
+    "dist:clean",
+    "dist:javascript",
+    "dist:css",
+    "dist:assets",
+    "dist:zip"
+  )
+);
