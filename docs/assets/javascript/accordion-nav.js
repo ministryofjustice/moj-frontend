77069c5eb62fbb28a44b9b082387f37f105a336c
Chris Pymm <chris@chrispymm.co.uk>
2024-09-23 08:36:22 +0100

    docs(side navigation): wIP - adding accordion behaviour to side nav

--- docs/assets/javascript/accordion-nav.js
+++
@@ -1,5 +0,0 @@
-export default class AccordionNav extends HTMLElement {
-  constructor() {
-    super()
-  }
-}
---
+++ docs/assets/javascript/collapsible-nav.js
@@ -0,0 +1,64 @@
+export default class CollapsibleNav extends HTMLElement {
+  constructor() {
+    super()
+
+    this.collapsibleItems = this.querySelectorAll('li:has(ul)')
+
+    this.collapsibleItems.forEach( (item, index) => {
+      const $link = item.querySelector('a')
+      const $list = item.querySelector('ul')
+      const $button = document.createElement('button')
+      const open = item.classList.contains(this.openClass)
+
+      if(!$list.id) {
+        $list.id = `moj-collapsible-nav-${index}`
+      }
+
+      $button.setAttribute('aria-controls', $list.id)
+      $button.setAttribute('aria-expanded', false)
+
+      $button.insertAdjacentHTML('afterbegin', $link.innerHTML)
+      item.replaceChild($button, $link)
+      if(!open) {
+        $list.hidden = true
+      }
+    })
+
+    this.addEventListener('click', this)
+  }
+
+  handleEvent(event) {
+    this[`on${event.type}`](event)
+  }
+
+  onclick(event) {
+    const $button = event.target.closest('button')
+    if(!$button) return
+    const $list = $button.parentNode.querySelector('ul')
+    if(!$list) return
+
+    if($list.hidden) {
+      this.closeAll();
+      $list.hidden = false;
+      $button.setAttribute('aria-expanded', true)
+    } else {
+      $list.hidden = true
+      $button.setAttribute('aria-expanded', false)
+    }
+  }
+
+  closeAll() {
+    this.openItems.forEach((item) => {
+      item.hidden = true
+    })
+  }
+
+  get openClass() {
+    return this.getAttribute('open-class') || 'is-open'
+  }
+
+  get openItems() {
+    return this.querySelectorAll('li ul:not([hidden])')
+  }
+
+}
