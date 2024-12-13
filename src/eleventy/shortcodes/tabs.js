686817c3198e063c32c21150e918b51927497f8d
John Barnard <john.barnard@justice.gov.uk>
2024-12-13 14:42:47 +0000

    Added roles

--- src/eleventy/shortcodes/tabs.js
+++ src/eleventy/shortcodes/tabs.js
@@ -10,34 +10,34 @@ module.exports = function (eleventyConfig) {
     const tabsList = tabsStorage.map((tab, index) => {
       const isSelected = index === 0 ? '--selected' : '';
       return `
-<li class="govuk-tabs__list-item${isSelected} app-navigation__item">
-    <a class="govuk-tabs__tab app-navigation__link app-navigation__link" href="#${tabId(tab)}">
-        ${tab.label}
-    </a>
-</li>
-`.trim();
+      <li class="govuk-tabs__list-item${isSelected} app-navigation__item" role="presentation">
+        <a class="govuk-tabs__tab app-navigation__link app-navigation__link" href="#${tabId(tab)}" role="tab" >
+          ${tab.label}
+        </a>
+      </li>
+    `.trim();
     }).join("\n").trim();
 
     const tabPanels = tabsStorage.map((tab, index) => {
       const isHidden = index === 0 ? '' : ' govuk-tabs__panel--hidden';
       return `
-<div class="govuk-tabs__panel${isHidden}" id="${tabId(tab)}">
-    ${tab.content}
-</div>
-`.trim();
+      <div class="govuk-tabs__panel${isHidden}" id="${tabId(tab)}" role="tabpanel">
+        ${tab.content}
+      </div>
+    `.trim();
     }).join("\n").trim();
 
     tabsStorage = [];
 
     return `
-<div class="govuk-tabs app-navigation no-govuk-tabs-styles" data-module="govuk-tabs">
-    <h2 class="govuk-tabs__title">${label}</h2>
-    <ul class="govuk-tabs__list app-navigation__list">
+    <div class="govuk-tabs app-navigation no-govuk-tabs-styles" data-module="govuk-tabs">
+      <h2 class="govuk-tabs__title">${label}</h2>
+      <ul class="govuk-tabs__list app-navigation__list" role="tabpanel">
         ${tabsList}
-    </ul>
-    ${tabPanels}
-</div>
-`.trim();
+      </ul>
+      ${tabPanels}
+    </div>
+  `.trim();
   });
 
   // Find and store govuk tab for above tabs
