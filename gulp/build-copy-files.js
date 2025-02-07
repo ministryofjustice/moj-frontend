e786b3833a28ac34b064026b8047e3481dc20148
Colin Rotherham <work@colinr.com>
2025-02-07 09:19:49 +0000

    chore: exclude all JavaScript from copy task

--- gulp/build-copy-files.js
+++ gulp/build-copy-files.js
@@ -4,16 +4,19 @@ gulp.task('build:copy-files', () => {
   return gulp
     .src(
       [
-        'src/**/*',
-        '!src/moj/all.js', // this will get built
+        'src/moj/assets/**',
+        'src/moj/filters/**',
+        'src/moj/vendor/**',
+        'src/moj/**/*.{md,njk,scss}',
+        'src/moj/init.js',
         'README.md',
-        '!**/.DS_Store',
-        '!src/README.md'
+        '!**/.DS_Store'
       ],
       {
         allowEmpty: true,
+        base: 'src/moj',
         encoding: false
       }
     )
-    .pipe(gulp.dest('package/'))
+    .pipe(gulp.dest('package/moj'))
 })
