aa2c2e1a20b4b22837c34a4f478aa2a451c5eaab
jbarnardmoj <john.barnard@justice.gov.uk>
2025-02-17 15:20:23 +0000

    Component details - remove field and skip questions button and added validation rules

--- schema/component-details.schema.js
+++ schema/component-details.schema.js
@@ -2,17 +2,27 @@ const Joi = require('joi')
 
 const schema = Joi.object({
   componentName: Joi.string()
+    .pattern(/^[A-Za-z\s]+$/, 'letters')
+    .max(26)
     .required()
+    .messages({
+      'string.empty': 'Enter the name of the component',
+      'string.pattern.name': 'Enter a name for the component using letters',
+      'string.max': 'Enter a name for the component using fewer letters'
+    })
     .label('What is the name of the component?'),
   componentOverview: Joi.string()
     .required()
+    .messages({
+      'string.empty': 'Enter an overview description of the component'
+    })
     .label('Add an overview description about the component'),
   howIsTheComponentUsed: Joi.string()
     .required()
-    .label('How is the component used in your service?'),
-  componentProblemSolved: Joi.string()
-    .required()
-    .label('What problem did the component solve?')
+    .messages({
+      'string.empty': 'Enter an answer for how the component is used currently'
+    })
+    .label('How is the component used in your service?')
 })
 
 module.exports = schema
