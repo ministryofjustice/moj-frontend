8f85287f9949a59515c86d645326eba9b026962e
jbarnardmoj <john.barnard@justice.gov.uk>
2025-02-19 10:54:38 +0000

    Assistive tech schema

--- schema/add-assistive-tech.schema.js
+++ schema/add-assistive-tech.schema.js
@@ -1,16 +1,57 @@
 const Joi = require('joi')
+const moment = require('moment')
 
 const schema = Joi.object({
   'testingDate-day': Joi.string()
-    .allow(null, ''),
+    .pattern(/^\d{1,2}$/)
+    .required()
+    .messages({
+      'any.required': 'The date of the testing must include a day',
+      'string.empty': 'The date of the testing must include a day',
+      'string.pattern.base': 'The date of the testing must be a real date'
+    }),
+
   'testingDate-month': Joi.string()
-    .allow(null, ''),
+    .pattern(/^\d{1,2}$/)
+    .required()
+    .messages({
+      'any.required': 'The date of the testing must include a month',
+      'string.empty': 'The date of the testing must include a month',
+      'string.pattern.base': 'The date of the testing must be a real date'
+    }),
+
   'testingDate-year': Joi.string()
-    .allow(null, ''),
-  issuesDiscovered: Joi.string()
-    .allow(null, ''),
+    .pattern(/^\d{4}$/)
+    .required()
+    .messages({
+      'any.required': 'The date of the testing must include a year',
+      'string.empty': 'The date of the testing must include a year',
+      'string.pattern.base': 'The date of the testing must be a real date'
+    }),
+
+  testingDate: Joi.string()
+    .custom((value, helpers) => {
+      if (!moment(value, 'YYYY-MM-DD', true).isValid()) {
+        return helpers.error('any.invalid', { message: 'The date of the testing must be a real date' });
+      }
+      if (moment(value).isAfter(moment())) {
+        return helpers.error('any.invalid', { message: 'The date of the testing must be today or in the past' });
+      }
+      return value;
+    })
+    .messages({
+      'any.invalid': '{{#message}}'
+    }),
+
+  issuesDiscovered: Joi.string().optional().allow(null, ''),
+
   accessibilityReport: Joi.string()
-    .allow(null, ''),
+    .allow('')
+    .pattern(/\.pdf$/i)
+    .messages({
+      'string.pattern.base': 'The selected file must be a PDF'
+    })
+    .optional()
 })
 
 module.exports = schema
