bffe38d0360920712f254db8f4f0e29c70cc56b6
jbarnardmoj <john.barnard@justice.gov.uk>
2025-02-21 10:35:38 +0000

    updated logic for previous page: set root url as relative url was too confusing and incorrect for subpages. if on subpage go to previous subpage. when the previous page has sub pages in the session use the largest number

--- helpers/previous-page.js
+++ helpers/previous-page.js
@@ -1,4 +1,5 @@
 const { COMPONENT_FORM_PAGES } = require("../config");
+const urlRoot = '/get-involved/add-new-component'
 
 const checkConditions = (conditions, session) => {
   return Object.entries(conditions).every(([key, value]) => {
@@ -11,8 +12,13 @@ const checkConditions = (conditions, session) => {
 
 const previousPage = (url, session) => {
   const pages = Object.keys(COMPONENT_FORM_PAGES);
-  const currentPage = url.split('/')[1] // ensure we get the page not the forward slash or any subpage detail
+  const [currentPage, subpage] = url.split('/').slice(1); // split the url to get the page and subpage
   const currentPageIndex = pages.findIndex(page => currentPage.endsWith(page));
+
+  // Check if there are subpages in the session for the current page
+  if (subpage && Number.isInteger(Number(subpage)) && subpage > 1) {
+    return `${urlRoot}/${currentPage}/${subpage - 1}`;
+  }
 
   for (let i = currentPageIndex - 1; i >= 0; i--) {
     const page = pages[i];
@@ -20,7 +26,16 @@ const previousPage = (url, session) => {
     const shouldShowPage = checkConditions(conditions, session);
 
     if (shouldShowPage) {
-      return page;
+      // Check if there are subpages in the session for the previous page
+      if (session) {
+        const subpages = Object.keys(session).filter(p => p.startsWith(`/${page}/`));
+        if (subpages.length > 0) {
+          // Sort subpages to get the highest one
+          subpages.sort((a, b) => b.localeCompare(a, undefined, { numeric: true }));
+          return `${urlRoot}${subpages[0]}`
+        }
+      }
+      return `${urlRoot}/${page}`;
     }
   }
 
