3b9d4890d93d61d1613021cae630b5586b248faf
Colin Rotherham <work@colinr.com>
2025-02-25 07:41:52 +0000

    fix: avoid Sass deprecated `mixed-decls`

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -24,7 +24,7 @@ function compileStyles(assetPath, { srcPath, destPath, output = {} }) {
     const { css, sourceMap } = await compileAsync(from, {
       loadPaths: ['.', 'node_modules'],
       quietDeps: true,
-      silenceDeprecations: ['import', 'mixed-decls'],
+      silenceDeprecations: ['import'],
       sourceMap: true,
       sourceMapIncludeSources: true
     })
