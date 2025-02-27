50be8396cb26621c474706a7fbda7b20fc69a1b1
Colin Rotherham <work@colinr.com>
2025-02-27 20:07:39 +0000

    feat: configure Babel for supported browsers

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
-  presets: ['@babel/preset-env']
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        // Apply bug fixes to avoid transforms
+        bugfixes: true,
+
+        // Apply smaller "loose" transforms for browsers
+        loose: true
+      }
+    ]
+  ],
+  env: {
+    test: {
+      browserslistEnv: 'node'
+    }
+  }
 }
