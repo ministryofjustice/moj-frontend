572bc454137a3b45541731313c3139339e7dec96
Colin Rotherham <work@colinr.com>
2025-02-28 17:32:43 +0000

    feat: export components as ES2015 classes

--- docs/assets/javascript/copy.mjs
+++ docs/assets/javascript/copy.mjs
@@ -1,42 +1,44 @@
 import ClipboardJS from 'clipboard'
 
-function Copy($module) {
-  this.$module = $module
-}
-
-Copy.prototype.init = function () {
-  const $module = this.$module
-  if (!$module) {
-    return
+class Copy {
+  constructor($module) {
+    this.$module = $module
   }
-  const $button = document.createElement('button')
-  $button.className = 'app-copy-button js-copy-button'
-  $button.setAttribute('aria-live', 'assertive')
-  $button.textContent = 'Copy code'
 
-  $module.insertBefore($button, $module.firstChild)
-  this.copyAction()
-}
+  init() {
+    const $module = this.$module
+    if (!$module) {
+      return
+    }
+    const $button = document.createElement('button')
+    $button.className = 'app-copy-button js-copy-button'
+    $button.setAttribute('aria-live', 'assertive')
+    $button.textContent = 'Copy code'
 
-Copy.prototype.copyAction = function () {
-  const $pre = this.$module.querySelector('pre')
+    $module.insertBefore($button, $module.firstChild)
+    this.copyAction()
+  }
 
-  // Copy to clipboard
-  try {
-    new ClipboardJS('.js-copy-button', {
-      target: function () {
-        return $pre
+  copyAction() {
+    const $pre = this.$module.querySelector('pre')
+
+    // Copy to clipboard
+    try {
+      new ClipboardJS('.js-copy-button', {
+        target: function () {
+          return $pre
+        }
+      }).on('success', function (e) {
+        e.trigger.textContent = 'Code copied'
+        e.clearSelection()
+        setTimeout(function () {
+          e.trigger.textContent = 'Copy code'
+        }, 5000)
+      })
+    } catch (err) {
+      if (err) {
+        console.log(err.message)
       }
-    }).on('success', function (e) {
-      e.trigger.textContent = 'Code copied'
-      e.clearSelection()
-      setTimeout(function () {
-        e.trigger.textContent = 'Copy code'
-      }, 5000)
-    })
-  } catch (err) {
-    if (err) {
-      console.log(err.message)
     }
   }
 }
--- src/moj/components/add-another/add-another.mjs
+++ src/moj/components/add-another/add-another.mjs
@@ -1,106 +1,108 @@
 import $ from 'jquery'
 
-export function AddAnother(container) {
-  this.container = $(container)
+export class AddAnother {
+  constructor(container) {
+    this.container = $(container)
 
-  if (this.container.data('moj-add-another-initialised')) {
-    return
-  }
-
-  this.container.data('moj-add-another-initialised', true)
-
-  this.container.on(
-    'click',
-    '.moj-add-another__remove-button',
-    $.proxy(this, 'onRemoveButtonClick')
-  )
-  this.container.on(
-    'click',
-    '.moj-add-another__add-button',
-    $.proxy(this, 'onAddButtonClick')
-  )
-  this.container
-    .find('.moj-add-another__add-button, moj-add-another__remove-button')
-    .prop('type', 'button')
-}
-
-AddAnother.prototype.onAddButtonClick = function (e) {
-  const item = this.getNewItem()
-  this.updateAttributes(this.getItems().length, item)
-  this.resetItem(item)
-  const firstItem = this.getItems().first()
-  if (!this.hasRemoveButton(firstItem)) {
-    this.createRemoveButton(firstItem)
-  }
-  this.getItems().last().after(item)
-  item.find('input, textarea, select').first().focus()
-}
-
-AddAnother.prototype.hasRemoveButton = function (item) {
-  return item.find('.moj-add-another__remove-button').length
-}
-
-AddAnother.prototype.getItems = function () {
-  return this.container.find('.moj-add-another__item')
-}
-
-AddAnother.prototype.getNewItem = function () {
-  const item = this.getItems().first().clone()
-  if (!this.hasRemoveButton(item)) {
-    this.createRemoveButton(item)
-  }
-  return item
-}
-
-AddAnother.prototype.updateAttributes = function (index, item) {
-  item.find('[data-name]').each(function (i, el) {
-    const originalId = el.id
-
-    el.name = $(el)
-      .attr('data-name')
-      .replace(/%index%/, index)
-    el.id = $(el)
-      .attr('data-id')
-      .replace(/%index%/, index)
-
-    const label =
-      $(el).siblings('label')[0] ||
-      $(el).parents('label')[0] ||
-      item.find(`[for="${originalId}"]`)[0]
-    label.htmlFor = el.id
-  })
-}
-
-AddAnother.prototype.createRemoveButton = function (item) {
-  item.append(
-    '<button type="button" class="govuk-button govuk-button--secondary moj-add-another__remove-button">Remove</button>'
-  )
-}
-
-AddAnother.prototype.resetItem = function (item) {
-  item.find('[data-name], [data-id]').each(function (index, el) {
-    if (el.type === 'checkbox' || el.type === 'radio') {
-      el.checked = false
-    } else {
-      el.value = ''
+    if (this.container.data('moj-add-another-initialised')) {
+      return
     }
-  })
-}
 
-AddAnother.prototype.onRemoveButtonClick = function (e) {
-  $(e.currentTarget).parents('.moj-add-another__item').remove()
-  const items = this.getItems()
-  if (items.length === 1) {
-    items.find('.moj-add-another__remove-button').remove()
+    this.container.data('moj-add-another-initialised', true)
+
+    this.container.on(
+      'click',
+      '.moj-add-another__remove-button',
+      $.proxy(this, 'onRemoveButtonClick')
+    )
+    this.container.on(
+      'click',
+      '.moj-add-another__add-button',
+      $.proxy(this, 'onAddButtonClick')
+    )
+    this.container
+      .find('.moj-add-another__add-button, moj-add-another__remove-button')
+      .prop('type', 'button')
   }
-  items.each(
-    $.proxy(function (index, el) {
-      this.updateAttributes(index, $(el))
-    }, this)
-  )
-  this.focusHeading()
-}
 
-AddAnother.prototype.focusHeading = function () {
-  this.container.find('.moj-add-another__heading').get(0).focus()
+  onAddButtonClick(e) {
+    const item = this.getNewItem()
+    this.updateAttributes(this.getItems().length, item)
+    this.resetItem(item)
+    const firstItem = this.getItems().first()
+    if (!this.hasRemoveButton(firstItem)) {
+      this.createRemoveButton(firstItem)
+    }
+    this.getItems().last().after(item)
+    item.find('input, textarea, select').first().focus()
+  }
+
+  hasRemoveButton(item) {
+    return item.find('.moj-add-another__remove-button').length
+  }
+
+  getItems() {
+    return this.container.find('.moj-add-another__item')
+  }
+
+  getNewItem() {
+    const item = this.getItems().first().clone()
+    if (!this.hasRemoveButton(item)) {
+      this.createRemoveButton(item)
+    }
+    return item
+  }
+
+  updateAttributes(index, item) {
+    item.find('[data-name]').each(function (i, el) {
+      const originalId = el.id
+
+      el.name = $(el)
+        .attr('data-name')
+        .replace(/%index%/, index)
+      el.id = $(el)
+        .attr('data-id')
+        .replace(/%index%/, index)
+
+      const label =
+        $(el).siblings('label')[0] ||
+        $(el).parents('label')[0] ||
+        item.find(`[for="${originalId}"]`)[0]
+      label.htmlFor = el.id
+    })
+  }
+
+  createRemoveButton(item) {
+    item.append(
+      '<button type="button" class="govuk-button govuk-button--secondary moj-add-another__remove-button">Remove</button>'
+    )
+  }
+
+  resetItem(item) {
+    item.find('[data-name], [data-id]').each(function (index, el) {
+      if (el.type === 'checkbox' || el.type === 'radio') {
+        el.checked = false
+      } else {
+        el.value = ''
+      }
+    })
+  }
+
+  onRemoveButtonClick(e) {
+    $(e.currentTarget).parents('.moj-add-another__item').remove()
+    const items = this.getItems()
+    if (items.length === 1) {
+      items.find('.moj-add-another__remove-button').remove()
+    }
+    items.each(
+      $.proxy(function (index, el) {
+        this.updateAttributes(index, $(el))
+      }, this)
+    )
+    this.focusHeading()
+  }
+
+  focusHeading() {
+    this.container.find('.moj-add-another__heading').get(0).focus()
+  }
 }
