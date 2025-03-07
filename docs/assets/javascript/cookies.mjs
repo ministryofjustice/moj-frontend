672359b14fdb40e24dbed7aae38eda82fd7bf5e2
Colin Rotherham <work@colinr.com>
2025-03-07 16:04:53 +0000

    feat: extend from GOV.UK Frontend components

--- docs/assets/javascript/cookies.mjs
+++ docs/assets/javascript/cookies.mjs
@@ -1,15 +1,13 @@
-class Cookies {
-  constructor($module) {
-    if (!$module) {
-      return
-    }
+import { Component } from 'govuk-frontend'
 
-    this.$module = $module
+class Cookies extends Component {
+  constructor($root) {
+    super($root)
 
-    const $accept = this.$module.querySelector('[name="accept"]')
+    const $accept = this.$root.querySelector('[name="accept"]')
     $accept.addEventListener('click', this.accept.bind(this))
 
-    const $reject = this.$module.querySelector('[name="reject"]')
+    const $reject = this.$root.querySelector('[name="reject"]')
     $reject.addEventListener('click', this.reject.bind(this))
 
     const configEncoded = localStorage.getItem('mojpl-cookies')
@@ -18,7 +16,7 @@ class Cookies {
       this.load(config)
     } else {
       // If there is no config, show the cookie banner
-      this.$module.hidden = false
+      this.$root.hidden = false
     }
   }
 
@@ -35,8 +33,8 @@ class Cookies {
   }
 
   hideMessage() {
-    if (!this.$module.hasAttribute('data-persistent')) {
-      this.$module.hidden = true
+    if (!this.$root.hasAttribute('data-persistent')) {
+      this.$root.hidden = true
     }
   }
 
@@ -53,6 +51,11 @@ class Cookies {
 
     window.location.reload()
   }
+
+  /**
+   * Name for the component used when initialising using data-module attributes.
+   */
+  static moduleName = 'app-cookies'
 }
 
 function gtag() {
