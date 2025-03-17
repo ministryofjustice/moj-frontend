aa3cf21e0d4d77d85404dd20069183c6547396b1
Colin Rotherham <work@colinr.com>
2025-03-17 13:55:38 +0000

    Disable TLS and security headers etc in development

--- app.js
+++ app.js
@@ -12,49 +12,53 @@ const RedisStore = require('connect-redis')(session)
 const helmet = require('helmet')
 const nunjucks = require('nunjucks')
 
-// Configure rate limiting
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 600, // Limit each IP to 200 requests per windowMs
-  standardHeaders: true,
-  legacyHeaders: false
-})
-
-const { APP_PORT, REDIS_URL, SESSION_SECRET } = require('./config')
+const { APP_PORT, ENV, REDIS_URL, SESSION_SECRET } = require('./config')
 const addComponentRoutes = require('./routes/add-component')
 
 const app = express()
-// Only trust single proxy (Nginx)
-app.set('trust proxy', 1)
-const isDev = app.get('env') === 'development'
+const isDev = ENV === 'development'
 
-app.use(
-  helmet({
-    contentSecurityPolicy: {
-      directives: {
-        'default-src': ["'self'"],
-        'connect-src': [
-          "'self'",
-          'https://www.googletagmanager.com', // Allow GTM
-          'https://region1.google-analytics.com' // Allow Google Analytics
-        ],
-        'script-src': [
-          "'self'",
-          'https://www.googletagmanager.com', // Allow GTM
-          "'sha256-GUQ5ad8JK5KmEWmROf3LZd9ge94daqNvd8xy9YS1iDw='", // Script modifying document.body.className
-          "'sha256-skTG6tpg3ly5Qkn5WY5737dUMy44jsVx49TxDFudrWg='" // Google tag manager
-        ],
-        'frame-src': [
-          "'self'",
-          'http://localhost:3001',
-          'https://design-patterns.service.justice.gov.uk/',
-          'https://*.cloud-platform.service.justice.gov.uk'
-        ] // Allow embedding
+if (!isDev) {
+  // Only trust single proxy (Nginx)
+  app.set('trust proxy', 1)
+
+  // Add security headers
+  app.use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          'default-src': ["'self'"],
+          'connect-src': [
+            "'self'",
+            'https://www.googletagmanager.com', // Allow GTM
+            'https://region1.google-analytics.com' // Allow Google Analytics
+          ],
+          'script-src': [
+            "'self'",
+            'https://www.googletagmanager.com', // Allow GTM
+            "'sha256-GUQ5ad8JK5KmEWmROf3LZd9ge94daqNvd8xy9YS1iDw='", // Script modifying document.body.className
+            "'sha256-skTG6tpg3ly5Qkn5WY5737dUMy44jsVx49TxDFudrWg='" // Google tag manager
+          ],
+          'frame-src': [
+            "'self'",
+            'https://design-patterns.service.justice.gov.uk/',
+            'https://*.cloud-platform.service.justice.gov.uk'
+          ] // Allow embedding
+        }
       }
-    }
-  })
-)
-app.use(limiter)
+    })
+  )
+
+  // Add rate limiting
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 600, // Limit each IP to 200 requests per windowMs
+      standardHeaders: true,
+      legacyHeaders: false
+    })
+  )
+}
 
 // Session management
 const sessionOptions = {
