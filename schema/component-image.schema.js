6b95dfd493d1ceb169294c942c4619aba9cfc480
jbarnardmoj <john.barnard@justice.gov.uk>
2025-03-18 14:27:23 +0000

    Corrected upload component image error message

--- schema/component-image.schema.js
+++ schema/component-image.schema.js
@@ -3,7 +3,9 @@ const Joi = require('joi')
 const addAnotherSchema = require('./add-another.schema')
 
 const schema = addAnotherSchema.append({
-  componentImage: Joi.string().required().label('Upload a file')
+  componentImage: Joi.string().required().label('Upload a file').messages({
+    'any.required': 'Select an image to upload'
+  })
 })
 
 module.exports = schema
