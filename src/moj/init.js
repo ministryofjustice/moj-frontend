e6af9888ba6314e244b3ce9549281f0d91c429a1
Colin Rotherham <work@colinr.com>
2025-03-25 20:07:40 +0000

    feat: configure GOV.UK Prototype Kit for ES modules

--- src/moj/init.js
+++ src/moj/init.js
@@ -1,5 +1,14 @@
-if (window.GOVUKPrototypeKit && window.GOVUKPrototypeKit.documentReady) {
-  window.GOVUKPrototypeKit.documentReady(function () {
+import * as MOJFrontend from '../moj/moj-frontend.min.js'
+
+// Maintain window global for compatibility
+window.MOJFrontend = MOJFrontend
+
+if (
+  window.GOVUKPrototypeKit &&
+  window.GOVUKPrototypeKit.documentReady &&
+  window.GOVUKPrototypeKit.majorVersion >= 13
+) {
+  window.GOVUKPrototypeKit.documentReady(() => {
     window.MOJFrontend.initAll()
   })
 }
