89d187a29207effd9f531f67645bfd16c1e1cc0d
Colin Rotherham <work@colinr.com>
2025-03-27 11:30:57 +0000

    docs: initialise accordions separately

--- docs/javascripts/application.mjs
+++ docs/javascripts/application.mjs
@@ -1,8 +1,18 @@
 /* eslint-disable no-new */
 
 import * as MOJFrontend from '@ministryofjustice/frontend'
-import * as GOVUKFrontend from 'govuk-frontend'
+import {
+  createAll,
+  Button,
+  CharacterCount,
+  Checkboxes,
+  ErrorSummary,
+  NotificationBanner,
+  Radios,
+  SkipLink
+} from 'govuk-frontend'
 
+import { initAccordions } from './accordions.mjs'
 import CollapsibleNav from './collapsible-nav.mjs'
 import Cookies from './cookies.mjs'
 import Copy from './copy.mjs'
@@ -10,7 +20,19 @@ import IFrameResizer from './iframe-resizer.mjs'
 import MenuToggle from './menu-toggle.mjs'
 import Tabs from './tabs.mjs'
 
-GOVUKFrontend.initAll()
+// GOV.UK Frontend components
+createAll(Button)
+createAll(CharacterCount)
+createAll(Checkboxes)
+createAll(ErrorSummary)
+createAll(NotificationBanner)
+createAll(Radios)
+createAll(SkipLink)
+
+const $accordions = document.querySelectorAll('[data-module="govuk-accordion"]')
+initAccordions($accordions)
+
+// MoJ Frontend components
 MOJFrontend.initAll()
 
 const $tabs = document.querySelectorAll('[data-module="app-tabs"]')
