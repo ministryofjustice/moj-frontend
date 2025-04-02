8b57d150c124cd582a081f41effaa70f629f9d7d
Chris Pymm <chris.pymm@digital.justice.gov.uk>
2025-04-02 21:39:00 +0100

    docs(community): add preview:request label to submitted pull request

--- helpers/get-pr-title-and-description.js
+++ helpers/get-pr-title-and-description.js
@@ -1,5 +1,6 @@
 const getPrTitleAndDescription = (session) => {
-  const componentName = session?.['/component-details']?.componentName || 'Unnamed component'
+  const componentName =
+    session?.['/component-details']?.componentName || 'Unnamed component'
   const title = `docs(contribution): ${componentName}`
   const description = session?.['/component-details']?.componentOverview || ''
   return { title, description }
