85ef6ba1e95f25841a844ba720f426f7368e340a
Chris Pymm <chris.pymm@digital.justice.gov.uk>
2025-05-13 14:24:33 +0100

    style: linting fixes

--- schema/add-internal-audit.schema.js
+++ schema/add-internal-audit.schema.js
@@ -85,9 +85,9 @@ const schema = Joi.object({
     .custom((value, helpers) => maxWords(value, helpers, 250))
     .messages({
       'any.required':
-        'Enter details about issues',
+        'Enter details about issues discovered by the internal review',
       'string.empty':
-        'Enter details about issues',
+        'Enter details about issues discovered by the internal review',
       'custom.max.words': 'Enter 250 words or less'
     })
 })
