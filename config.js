7087e028b15bf8bf1c72765c5705ec407570138d
Chris Pymm <chris@chrispymm.co.uk>
2025-05-21 14:21:01 +0100

    docs(contributions): Refactor routing conditions [1] (#1437)
    * style: linting fixes
    
    * docs(contribution): update form config to contain all labels [2] (#1438)
    
    * docs: add sentry error logging
    
    * build: add rollup optional dependency
    
    * build: add optional sass-embedded dependency
    
    * build: move sentry test route
    
    * docs(contribution): update form config to contain all labels
    
    * docs (contributions): Use field labels on cya page [3] (#1440)
    
    * build: move sentry test route
    
    * Remove accessibility report uploads (#1430)
    
    * docs(contributions): remove audit report file uploads from ui
    
    * docs(contributions): update accessibility schemas to make issues required
    
    * Update add-assistive-tech.schema.js
    
    New error message for AT testing page.
    
    * Update add-external-audit.schema.js
    
    Revise error for the external audit details field.
    
    * Update add-internal-audit.schema.js
    
    Revised error message.
    
    ---------
    
    Co-authored-by: Helen Nickols <94117270+helennickols@users.noreply.github.com>
    
    * docs(contribution): use labels from config for cya page
    
    * docs(contribition): remove overrides
    
    * docs(comtributions): update cya page config with section titles
    
    * docs(contributions): refactor cya config to include / exclude fields and combine fields and forms
    
    * docs: update field value [4] (#1447)
    
    * docs: change input to radio button
    
    * Update component-code-details.njk
    
    * docs: changing 'other code' to required
    
    * docs: content updates
    
    * docs(contributions): update new code details to use config for labels
    
    * docs: Security fixes [5] (#1464)
    
    * docs(security): ensure pages are valid and subpages are numbers
    
    * docs(security): fix potential prototype pollution issue
    
    * docs: refactor template and pagedata into methods
    
    * style: linting fixes
    
    ---------
    
    Co-authored-by: Chris Pymm <chris.pymm@digital.justice.gov.uk>
    Co-authored-by: Chris Pymm <chris@chrispymm.co.uk>
    
    ---------
    
    Co-authored-by: Helen Nickols <94117270+helennickols@users.noreply.github.com>
    Co-authored-by: Murray Lippiatt <58863567+murrlipp@users.noreply.github.com>
    
    * docs: review fixes
    
    * style: lint fixes
    
    * docs: fix redirect after image upload
    
    ---------
    
    Co-authored-by: Murray Lippiatt <murray.lippiatt1@digital.justice.gov.uk>
    Co-authored-by: Helen Nickols <94117270+helennickols@users.noreply.github.com>
    Co-authored-by: Murray Lippiatt <58863567+murrlipp@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Murray Lippiatt <murray.lippiatt1@digital.justice.gov.uk>
    Co-authored-by: Helen Nickols <94117270+helennickols@users.noreply.github.com>
    Co-authored-by: Murray Lippiatt <58863567+murrlipp@users.noreply.github.com>

--- config.js
+++ config.js
@@ -18,10 +18,57 @@ const config = {
   SESSION_SECRET: process.env.SESSION_SECRET || 'your-secret-key',
   ENV: process.env.ENV || 'development',
   COMPONENT_FORM_PAGES: {
-    'component-details': {},
-    'component-image': {},
-    'accessibility-findings': {},
+    'component-details': {
+      fields: {
+        componentName: {
+          label: 'What’s the name of the component?'
+        },
+        componentOverview: {
+          label: 'Describe the component'
+        },
+        howIsTheComponentUsed: {
+          label: 'How is the component being used?'
+        }
+      }
+    },
+    'component-image': {
+      fields: {
+        componentImage: {
+          label: 'Upload a file',
+          hint: 'The file must be a JPG, BMP, PNG, TIF or PDF, and smaller than 10MB.'
+        }
+      }
+    },
+    'accessibility-findings': {
+      fields: {
+        hasComponentBeenTestedExternalAccessibility: {
+          label:
+            'Has the component been tested in an external accessibility audit?',
+          hint: 'This would be from a third party organisation.'
+        },
+        hasComponentBeenTestedInternalAudit: {
+          label: 'Has the component been reviewed internally?',
+          hint: 'This would be within UK Government.'
+        },
+        hasComponentBeenTestedUsingAssistiveTechnology: {
+          label: 'Has the component been tested with assistive technology?',
+          hint: 'This could be with screen reader software or an adaptive keyboard.'
+        }
+      }
+    },
     'add-external-audit': {
+      fields: {
+        externalOrganisation: {
+          label: 'Which organisation did the external accessibility audit?'
+        },
+        auditDate: {
+          label: 'When was the audit?',
+          hint: 'For example, 13 8 2024'
+        },
+        issuesDiscovered: {
+          label: 'Enter details about issues discovered by the external audit'
+        }
+      },
       conditions: {
         '/accessibility-findings': {
           hasComponentBeenTestedExternalAccessibility: 'yes'
@@ -29,6 +76,18 @@ const config = {
       }
     },
     'add-internal-audit': {
+      fields: {
+        internalOrganisation: {
+          label: 'What team did the internal accessibility review?'
+        },
+        auditDate: {
+          label: 'When was the review?',
+          hint: 'For example, 13 8 2024'
+        },
+        issuesDiscovered: {
+          label: 'Enter details about issues discovered by the internal review'
+        }
+      },
       conditions: {
         '/accessibility-findings': {
           hasComponentBeenTestedInternalAudit: 'yes'
@@ -36,64 +95,179 @@ const config = {
       }
     },
     'add-assistive-tech': {
+      fields: {
+        testingDate: {
+          label: 'When was the component tested with assistive technology?',
+          hint: 'For example, 13 8 2024'
+        },
+        issuesDiscovered: {
+          label:
+            'Enter details about issues discovered by the assistive technology testing'
+        }
+      },
       conditions: {
         '/accessibility-findings': {
           hasComponentBeenTestedUsingAssistiveTechnology: 'yes'
         }
       }
     },
-    prototype: {},
+    prototype: {
+      fields: {
+        componentPrototypeUrl: {
+          label: 'Do you have a prototype link?',
+          hint: 'For example to the GOV.UK Prototype Kit, Figma, Sketch, Mural or Miro.'
+        }
+      }
+    },
     'prototype-url': {
+      fields: {
+        prototypeUrl: {
+          label: 'Add the prototype link',
+          hint: 'For example, to the GOV.UK Prototype Kit, Figma, Sketch, Mural or Miro. '
+        },
+        prototypeUrlAdditionalInformation: {
+          label: 'Add more information about the prototype (optional)',
+          hint: 'Enter any relevant information, for example a username, password or link expiry date.'
+        }
+      },
       conditions: {
         '/prototype': { componentPrototypeUrl: 'yes' }
       }
     },
-    'component-code': {},
+    'component-code': {
+      fields: {
+        componentCodeAvailable: {
+          label: 'Do you have code for the component?'
+        }
+      }
+    },
     'component-code-details': {
+      fields: {
+        componentCodeLanguage: {
+          label: 'Select the code language',
+          hint: 'If you have code in 2 or more languages you can add another code example.'
+        },
+        componentCodeLanguageOther: {
+          label: 'Enter the code language'
+        },
+        componentCodeUsage: {
+          label: 'How do you use the code? (optional)',
+          hint: 'Include anything that helps users add the code to their service.'
+        },
+        componentCode: {
+          label: 'Add the code',
+          hint: 'Add the code for the component only (not the full page) without any personally identifiable or sensitive information.'
+        }
+      },
       conditions: {
         '/component-code': { componentCodeAvailable: 'yes' }
       }
     },
-    figma: {},
+    figma: {
+      fields: {
+        figmaUrl: {
+          label: 'Do you have a Figma link for the component?'
+        }
+      }
+    },
     'figma-link': {
+      fields: {
+        figmaLink: {
+          label: 'Add a link to the Figma design file',
+          hint: 'Set the sharing permissions so that it can be viewed.'
+        },
+        figmaLinkAdditionalInformation: {
+          label: 'Add information about the Figma design file (optional)',
+          hint: 'This might include the password for a protected file, or a link expiry date.'
+        }
+      },
       conditions: {
         '/figma': { figmaUrl: 'yes' }
       }
     },
-    'your-details': {},
+    'your-details': {
+      fields: {
+        fullName: {
+          label: 'Full name'
+        },
+        emailAddress: {
+          label: 'Email address'
+        },
+        teamName: {
+          label: 'What team were you in when this component was created?'
+        },
+        shareYourDetails: {
+          label: 'Sharing your contact details on the component page',
+          hint: 'Adding your details to the component enables other users to contact you (or your team) to collaborate on the component.\r\nSelect all that apply'
+        }
+      }
+    },
     'check-your-answers': {}
   },
   ADD_NEW_COMPONENT_ROUTE: '/contribute/add-new-component',
   MAX_ADD_ANOTHER: 10,
   ACRONYMS_TO_UPPERCASE: ['url'],
-  CHECK_YOUR_ANSWERS_LABEL_MAPPING: {
-    componentOverview: 'Component description',
-    howIsTheComponentUsed: 'How the component is being used in your service',
-    accessibilityReport: 'Audit report',
-    externalOrganisation: 'External accessibility audit supplier',
-    internalOrganisation: 'Internal accessibility audit supplier',
-    issuesDiscovered: 'Issues discovered during the audit',
-    prototypeUrl: 'Prototype link',
-    prototypeUrlAdditionalInformation: 'Additional information',
-    componentCode: 'Code',
-    componentCodeUsage: 'Code usage',
-    componentCodeLanguage: 'Code language',
-    figmaLink: 'Prototype link',
-    figmaLinkAdditionalInformation: 'Additional information',
-    shareYourDetails: 'Share your details on the component page',
-    componentImage: 'Supporting file'
+  CHECK_YOUR_ANSWERS_LABEL_OVERRIDES: {
+    // componentOverview: 'Component description',
+    // howIsTheComponentUsed: 'How the component is being used in your service',
+    // accessibilityReport: 'Audit report',
+    // externalOrganisation: 'External accessibility audit supplier',
+    // internalOrganisation: 'Internal accessibility audit supplier',
+    // issuesDiscovered: 'Issues discovered during the audit',
+    // prototypeUrl: 'Prototype link',
+    // prototypeUrlAdditionalInformation: 'Additional information',
+    // componentCode: 'Code',
+    // componentCodeUsage: 'Code usage',
+    // componentCodeLanguage: 'Code language',
+    // componentCodeLanguageOther: 'Other code language',
+    // figmaLink: 'Prototype link',
+    // figmaLinkAdditionalInformation: 'Additional information',
+    // shareYourDetails: 'Share your details on the component page',
+    // componentImage: 'Supporting file'
   },
   CHECK_YOUR_ANSWERS: {
-    forms: [
-      'component-details',
-      'component-image',
-      'add-external-audit',
-      'add-internal-audit',
-      'add-assistive-tech',
-      ['prototype', 'prototype-url'],
-      ['figma', 'figma-link'],
-      ['component-code', 'component-code-details'],
-      'your-details'
+    sections: [
+      {
+        title: 'Component details',
+        data: ['component-details', 'component-image']
+      },
+      {
+        title: 'External accessibility audit',
+        data: [
+          {
+            'accessibility-findings': {
+              includeFields: ['hasComponentBeenTestedExternalAccessibility']
+            }
+          },
+          'add-external-audit'
+        ]
+      },
+      {
+        title: 'Internal accessibility review',
+        data: [
+          {
+            'accessibility-findings': {
+              includeFields: ['hasComponentBeenTestedInternalAudit']
+            }
+          },
+          'add-internal-audit'
+        ]
+      },
+      {
+        title: 'Assistive technology testing',
+        data: [
+          {
+            'accessibility-findings': {
+              includeFields: ['hasComponentBeenTestedUsingAssistiveTechnology']
+            }
+          },
+          'add-assistive-tech'
+        ]
+      },
+      { title: 'Prototype', data: ['prototype', 'prototype-url'] },
+      { title: 'Figma', data: ['figma', 'figma-link'] },
+      { title: 'Code', data: ['component-code', 'component-code-details'] },
+      { title: 'Your details', data: ['your-details'] }
     ],
     canRemoveStatic: [
       '/accessibility-findings',
@@ -110,11 +284,7 @@ const config = {
       '/figma-link',
       '/component-code-details'
     ],
-    ignoreFields: [
-      // 'componentPrototypeUrl',
-      // 'figmaUrl',
-      // 'componentCodeAvailable'
-    ]
+    ignoreFields: []
   },
   SHARE_YOUR_DETAILS: {
     addNameToComponentPage: 'Add name and email address to component page',
