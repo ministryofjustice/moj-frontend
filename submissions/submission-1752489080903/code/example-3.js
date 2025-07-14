8be066de303c0ea7f35e30f2bced6feb66e45184
Chris Pymm <chris@chrispymm.co.uk>
2025-07-14 11:31:40 +0100

    Add submissions/submission-1752489080903/code/example-3.js in submission folder

---
+++ submissions/submission-1752489080903/code/example-3.js
@@ -0,0 +1,8 @@
+function setLanguageOptions() {
+    var href = document.location.href,
+        deflt = href.substring(href.indexOf("default=")+8);
+    document.write("");
+    document.write("");
+}
+
+setLanguageOptions()
