be0eb374384d1dc08ae3ef4aae5096d61f7a0529
Chris Pymm <chris@chrispymm.co.uk>
2025-07-14 11:38:44 +0100

    Add submissions/submission-1752489503465/code/example-3.js in submission folder

---
+++ submissions/submission-1752489503465/code/example-3.js
@@ -0,0 +1,28 @@
+    const csrf_token = document.querySelector("meta[name='csrf-token']").getAttribute("content");
+
+    const ajaxLoad = () =&gt; {
+        // process the URL hash fragment
+        const hashFragment = window.location.hash.slice(1);
+
+        // hash fragment should be of the format: /^(get|post);(.*)$/
+        // e.g., https://site.com/index/#post;/profile
+        if (hashFragment.length &gt; 0 &amp;&amp; hashFragment.includes(';')) {
+            const params = hashFragment.match(/^(get|post);(.*)$/);
+
+            if (params &amp;&amp; params.length) {
+                const requestMethod = params[1];
+                const requestEndpoint = params[3];
+
+                fetch(requestEndpoint, {
+                    method: requestMethod,
+                    headers: {
+                        'X-CSRF-Token': csrf_token,
+                    },
+                })
+                .then(response =&gt; {  })
+                .catch(error =&gt; console.error('Request failed:', error));
+            }
+        }
+    };
+
+    window.addEventListener('DOMContentLoaded', ajaxLoad);
