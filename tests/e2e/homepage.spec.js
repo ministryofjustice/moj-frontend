d0d37e958c87571f7a210bc0682d0c20a6c2a6f5
Chris Pymm <chris@chrispymm.co.uk>
2025-10-08 16:22:23 +0100

    test: add e2e tests for design system docs site (#1872)
    * test: add tests for documentation site
    
    * test: add tests for design system docs site functionality
    
    * style: linting fixes

---
+++ tests/e2e/homepage.spec.js
@@ -0,0 +1,65 @@
+import { test, expect } from '@playwright/test'
+
+test('homepage', async ({ page }) => {
+  await page.goto('/')
+  await expect(page).toHaveTitle(/MoJ Design System/)
+  await expect(
+    page.getByRole('heading', {
+      name: 'Design, build, and deliver accessible and consistent services'
+    })
+  ).toBeVisible()
+})
+
+test('submit a component is visible', async ({ page }) => {
+  await page.goto('/')
+  const main = page.getByRole('main')
+  await expect(
+    page
+      .getByLabel('About the Design System')
+      .getByRole('link', { name: 'Submit a Component' })
+  ).toBeVisible()
+  await expect(
+    main.getByRole('link', { name: 'Submit a Component' })
+  ).toBeVisible()
+})
+
+test.describe('navigation', () => {
+  test('menus are collapsed', async ({ page }) => {
+    await page.goto('/')
+    await expect(
+      page.getByRole('link', { name: 'Date picker' })
+    ).not.toBeVisible()
+    await expect(
+      page.getByRole('link', { name: 'Filter a list' })
+    ).not.toBeVisible()
+    await expect(
+      page.getByRole('link', { name: 'Use Nunjucks' })
+    ).not.toBeVisible()
+  })
+
+  test('menus can be opened', async ({ page }) => {
+    await page.goto('/')
+
+    await page.getByRole('button', { name: 'Components' }).click()
+    await expect(page.getByRole('link', { name: 'Date picker' })).toBeVisible()
+
+    await page.getByRole('button', { name: 'Patterns' }).click()
+    await expect(
+      page.getByRole('link', { name: 'Date picker' })
+    ).not.toBeVisible()
+    await expect(
+      page.getByRole('link', { name: 'Filter a list' })
+    ).toBeVisible()
+
+    await page.getByRole('button', { name: 'Production' }).click()
+    await expect(
+      page.getByRole('link', { name: 'Filter a list' })
+    ).toBeVisible()
+    await expect(page.getByRole('link', { name: 'Use Nunjucks' })).toBeVisible()
+
+    await page.getByRole('button', { name: 'Production' }).click()
+    await expect(
+      page.getByRole('link', { name: 'Use Nunjucks' })
+    ).not.toBeVisible()
+  })
+})
