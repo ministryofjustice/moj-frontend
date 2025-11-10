4cc3d27e2fc1e4598e62ce832f6ca178d29e2d3a
Chris Pymm <chris.pymm@digital.justice.gov.uk>
2025-11-10 21:46:25 +0000

    style: lint fix

--- docs/components/add-another/add-another.11tydata.js
+++ docs/components/add-another/add-another.11tydata.js
@@ -1,4 +1,5 @@
 export default {
-  figma_link: 'https://www.figma.com/design/N2xqOFkyehXwcD9DxU2gEq/MoJ-Figma-Kit?node-id=7346-1848',
+  figma_link:
+    'https://www.figma.com/design/N2xqOFkyehXwcD9DxU2gEq/MoJ-Figma-Kit?node-id=7346-1848',
   githuburl: 'https://github.com/ministryofjustice/moj-frontend/discussions/686'
 }
--- docs/components/date-picker/date-picker.11tydata.js
+++ docs/components/date-picker/date-picker.11tydata.js
@@ -1,4 +1,5 @@
 export default {
-  figma_link: 'https://www.figma.com/design/N2xqOFkyehXwcD9DxU1gEq/MoJ-Figma-Kit?node-id=792-861&t=6DfPOX7RAnjrVE0j-0',
+  figma_link:
+    'https://www.figma.com/design/N2xqOFkyehXwcD9DxU1gEq/MoJ-Figma-Kit?node-id=792-861&t=6DfPOX7RAnjrVE0j-0',
   githuburl: 'https://github.com/ministryofjustice/moj-frontend/discussions/703'
 }
