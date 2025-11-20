a2c849e6771b158011df30dd28bbc2e3d3a826dc
Chris Pymm <chris@chrispymm.co.uk>
2025-11-20 06:49:43 +0000

    Add docs/components/test-no-figma/test-no-figma.11tydata.js in submission folder

---
+++ docs/components/test-no-figma/test-no-figma.11tydata.js
@@ -0,0 +1,5 @@
+export default {
+  githuburl: 'https://github.com/ministryofjustice/moj-frontend/discussions/categories/experimental-components-pages-and-patterns',
+  tabCollection: 'test-no-figma',
+  blockTitle: 'Test no figma'
+}
