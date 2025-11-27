74cb02b01d0ff6fe6e6e63dadd0352ad8a9f2c14
chrispymm <595564+chrispymm@users.noreply.github.com>
2025-11-27 17:11:33 +0000

    chore: fix linting issues

--- docs/components/api-error/api-error.11tydata.js
+++ docs/components/api-error/api-error.11tydata.js
@@ -1,5 +1,6 @@
 export default {
-  githuburl: 'https://github.com/ministryofjustice/moj-frontend/discussions/categories/experimental-components-pages-and-patterns',
+  githuburl:
+    'https://github.com/ministryofjustice/moj-frontend/discussions/categories/experimental-components-pages-and-patterns',
   tabCollection: 'api-error',
   blockTitle: 'Api error'
 }
