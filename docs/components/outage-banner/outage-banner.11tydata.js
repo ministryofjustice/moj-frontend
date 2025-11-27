7ddb9563477f03175f4d1b64e30eed1944cf9963
chrispymm <595564+chrispymm@users.noreply.github.com>
2025-11-27 16:32:18 +0000

    chore: fix linting issues

--- docs/components/outage-banner/outage-banner.11tydata.js
+++ docs/components/outage-banner/outage-banner.11tydata.js
@@ -1,5 +1,6 @@
 export default {
-  githuburl: 'https://github.com/ministryofjustice/moj-frontend/discussions/categories/experimental-components-pages-and-patterns',
+  githuburl:
+    'https://github.com/ministryofjustice/moj-frontend/discussions/categories/experimental-components-pages-and-patterns',
   tabCollection: 'outage-banner',
   blockTitle: 'Outage banner'
 }
