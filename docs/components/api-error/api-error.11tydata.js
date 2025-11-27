9f83997d8d58b05627107dc04431535011d0b353
Chris Pymm <chris@chrispymm.co.uk>
2025-11-27 17:10:12 +0000

    Add docs/components/api-error/api-error.11tydata.js in submission folder

---
+++ docs/components/api-error/api-error.11tydata.js
@@ -0,0 +1,5 @@
+export default {
+  githuburl: 'https://github.com/ministryofjustice/moj-frontend/discussions/categories/experimental-components-pages-and-patterns',
+  tabCollection: 'api-error',
+  blockTitle: 'Api error'
+}
