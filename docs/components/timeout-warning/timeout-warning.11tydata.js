8dd412806bc9ed76e5e64c8acbcde3d9ed1f211e
Murray Lippiatt <murray.lippiatt1@digital.justice.gov.uk>
2025-11-27 13:23:59 +0000

    docs: github discussion link

--- docs/components/timeout-warning/timeout-warning.11tydata.js
+++ docs/components/timeout-warning/timeout-warning.11tydata.js
@@ -1,5 +1,5 @@
 export default {
-  githuburl: 'https://github.com/ministryofjustice/moj-frontend/discussions/categories/experimental-components-pages-and-patterns',
+  githuburl: 'https://github.com/ministryofjustice/moj-frontend/discussions/726',
   tabCollection: 'timeout-warning',
   blockTitle: 'Timeout warning'
 }
