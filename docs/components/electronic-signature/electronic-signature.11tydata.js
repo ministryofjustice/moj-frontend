3c27f769bbea1709188a19f75fc0896e43485e4f
chrispymm <595564+chrispymm@users.noreply.github.com>
2025-12-08 13:50:21 +0000

    chore: fix linting issues

--- docs/components/electronic-signature/electronic-signature.11tydata.js
+++ docs/components/electronic-signature/electronic-signature.11tydata.js
@@ -1,6 +1,8 @@
 export default {
-  figma_link: 'https://www.figma.com/design/N2xqOFkyehXwcD9DxU1gEq/MOJ-Figma-Kit?m=auto',
-  githuburl: 'https://github.com/ministryofjustice/moj-frontend/discussions/categories/experimental-components-pages-and-patterns',
+  figma_link:
+    'https://www.figma.com/design/N2xqOFkyehXwcD9DxU1gEq/MOJ-Figma-Kit?m=auto',
+  githuburl:
+    'https://github.com/ministryofjustice/moj-frontend/discussions/categories/experimental-components-pages-and-patterns',
   tabCollection: 'electronic-signature',
   blockTitle: 'Electronic signature'
 }
