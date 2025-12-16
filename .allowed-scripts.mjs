a47a659329b3ddbcf307a6546981e3feecd275aa
Chris Pymm <chris.pymm@digital.justice.gov.uk>
2025-12-16 11:48:44 +0000

    chore: tweak allowlist

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -6,13 +6,13 @@ export default configureAllowedScripts({
     'node_modules/@sentry/cli@2.55.0': 'ALLOW',
     'node_modules/core-js@3.40.0': 'FORBID',
     'node_modules/gifsicle@5.3.0': 'ALLOW',
-    'node_modules/govuk-prototype-kit/node_modules/@parcel/watcher@2.5.1':
-      'ALLOW',
-    'node_modules/govuk-prototype-kit/node_modules/fsevents@2.3.2': 'ALLOW',
     'node_modules/mozjpeg@8.0.0': 'ALLOW',
     'node_modules/optipng-bin@7.0.1': 'ALLOW',
     'node_modules/playwright/node_modules/fsevents@2.3.2': 'ALLOW',
     'node_modules/puppeteer@24.22.3': 'ALLOW',
-    'node_modules/unrs-resolver@1.11.1': 'ALLOW'
+    'node_modules/unrs-resolver@1.11.1': 'ALLOW',
+    'node_modules/govuk-prototype-kit/node_modules/@parcel/watcher@2.5.1':
+      'ALLOW',
+    'node_modules/govuk-prototype-kit/node_modules/fsevents@2.3.2': 'ALLOW'
   }
 })
