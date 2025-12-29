04a100c3673f1dfb0c4d8f68baa9bb8e6fdc7fe0
Murray Lippiatt <murray.lippiatt1@digital.justice.gov.uk>
2025-12-29 16:45:33 +0000

    docs: add github URL

--- docs/components/outage-banner/outage-banner.11tydata.js
+++ docs/components/outage-banner/outage-banner.11tydata.js
@@ -1,6 +1,6 @@
 export default {
   githuburl:
-    'https://github.com/ministryofjustice/moj-frontend/discussions/categories/experimental-components-pages-and-patterns',
+    'https://github.com/ministryofjustice/moj-frontend/discussions/2115',
   tabCollection: 'outage-banner',
   blockTitle: 'Outage banner'
 }
