0dba7bac133d2aab89070ad665c195989523722c
Chris Pymm <chris@chrispymm.co.uk>
2026-02-13 22:31:22 +0000

    docs: fix figma link on add another (#2179)
    Fixes the broken Figma link on the add another component

--- docs/components/add-another/add-another.11tydata.js
+++ docs/components/add-another/add-another.11tydata.js
@@ -1,6 +1,6 @@
 export default {
   figma_link:
-    'https://www.figma.com/design/N2xqOFkyehXwcD9DxU2gEq/MoJ-Figma-Kit?node-id=7346-1848',
+    'https://www.figma.com/design/N2xqOFkyehXwcD9DxU1gEq/MOJ-Figma-Kit?node-id=7346-1848',
   githuburl:
     'https://github.com/ministryofjustice/moj-frontend/discussions/686',
   blockTitle: 'Add another'
