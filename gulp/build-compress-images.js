fd8187db9268a81572166db89e9dcce01c5e6ddb
Chris Pymm <chris.pymm@digital.justice.gov.uk>
2026-04-27 16:29:32 +0100

    build: replace gulp-immagemin with sharp

--- gulp/build-compress-images.js
+++ gulp/build-compress-images.js
@@ -1,20 +1,47 @@
+const { Transform } = require('stream')
+
 const gulp = require('gulp')
-const cache = require('gulp-cache')
+const sharp = require('sharp')
+const { optimize } = require('svgo')
 
-// Compresses png, jpeg, gif, png and svg images
-gulp.task('build:compress-images', async () => {
-  const { default: imagemin } = await import('gulp-imagemin')
+function compressImages() {
+  return new Transform({
+    objectMode: true,
+    async transform(file, _, callback) {
+      if (file.isNull()) return callback(null, file)
 
+      const ext = file.extname.toLowerCase()
+
+      if (ext === '.svg') {
+        const result = optimize(file.contents.toString(), { multipass: true })
+        file.contents = Buffer.from(result.data)
+        return callback(null, file)
+      }
+
+      try {
+        let pipeline = sharp(file.contents)
+
+        if (ext === '.png') {
+          pipeline = pipeline.png({ compressionLevel: 9 })
+        } else if (ext === '.jpg' || ext === '.jpeg') {
+          pipeline = pipeline.jpeg({ quality: 80 })
+        }
+
+        file.contents = await pipeline.toBuffer()
+        return callback(null, file)
+      } catch (err) {
+        return callback(err)
+      }
+    }
+  })
+}
+
+// Compresses png, jpeg and svg images
+gulp.task('build:compress-images', () => {
   return gulp
-    .src('package/moj/assets/images/**/*.+(png|jpg|jpeg|gif|svg)', {
+    .src('package/moj/assets/images/**/*.+(png|jpg|jpeg|svg)', {
       encoding: false
     })
-    .pipe(
-      cache(
-        imagemin({
-          interlaced: true
-        })
-      )
-    )
+    .pipe(compressImages())
     .pipe(gulp.dest('package/moj/assets/images/'))
 })
