45a38ceb977627eb79e61379d160cbb56b86e9a5
Chris Pymm <chris.pymm@digital.justice.gov.uk>
2026-04-28 06:43:15 +0100

    chore: migrate from ioredis and ioredis-mock
    connect-redis 9 no longer supports ioredis, migrate to node-redis and redis-mock

--- app/redis-client.js
+++ app/redis-client.js
@@ -1,4 +1,4 @@
-const IORedis = require('ioredis')
+const { createClient } = require('redis')
 
 const { REDIS_URL, REDIS_AUTH_TOKEN, REDIS_PORT, ENV } = require('./config')
 
@@ -6,17 +6,19 @@ let redis
 
 if (ENV === 'development' || ENV === 'test') {
   // Mock Redis client for development
-  const Redis = require('ioredis-mock')
-  redis = new Redis()
+  const redisMock = require('redis-mock')
+  redis = redisMock.createClient()
 } else {
   // Create Redis client
-  redis = new IORedis({
+  redis = createClient({
     host: REDIS_URL || '127.0.0.1',
     port: REDIS_PORT || 6379,
     ...(REDIS_AUTH_TOKEN
       ? { password: REDIS_AUTH_TOKEN, tls: {} }
       : { tls: false })
   })
+
+  redis.connect()
 
   redis.on('connect', () => console.log('[Redis] Connected'))
   redis.on('error', (err) => console.error('[Redis] Error:', err))
