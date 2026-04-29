bd0b550676c5d170bf2da46ba796b09e539ba3e2
Chris Pymm <chris@chrispymm.co.uk>
2026-04-29 15:47:35 +0100

    build: replace gulp-immagemin with sharp (#2271)
    The `gulp-imagemin` package has dependencies that are old and have
    security issues.
    
    The package itself is no longer actively maintained and a lot of its
    plugins are abandoned.
    
    This PR replaces gulp-imagemin with the 'sharp' image transformation
    library - this is widely used and under active development which is much
    better from a security standpoint.
    
    N.B. We have no gif images, and are unlikely to in the future so this
    new pipeline doesn't handle them.

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -4,15 +4,12 @@ export default configureAllowedScripts({
   allowlist: {
     'node_modules/@parcel/watcher@2.5.6': 'FORBID',
     'node_modules/@sentry/cli@2.58.5': 'ALLOW',
-    'node_modules/core-js@3.49.0': 'FORBID',
     'node_modules/fsevents@2.3.3': 'FORBID',
-    'node_modules/gifsicle@5.3.0': 'ALLOW',
-    'node_modules/mozjpeg@8.0.0': 'ALLOW',
-    'node_modules/optipng-bin@7.0.1': 'ALLOW',
     'node_modules/playwright/node_modules/fsevents@2.3.2': 'ALLOW',
     'node_modules/unrs-resolver@1.11.1': 'ALLOW',
     'node_modules/govuk-prototype-kit/node_modules/@parcel/watcher@2.5.1':
       'FORBID',
+    'node_modules/sharp@0.34.5': 'ALLOW',
     'node_modules/govuk-prototype-kit/node_modules/fsevents@2.3.2': 'FORBID'
   }
 })
