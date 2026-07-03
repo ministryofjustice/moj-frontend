725ea9a6548b2a16da3683e81dc4c1c4d34f156e
RoseBrandle <rose.brandle@justice.gov.uk>
2026-07-03 16:15:30 +0100

    Update pds-page-templates.11tydata.js

--- docs/probation/styles/pds-page-templates/pds-page-templates.11tydata.js
+++ docs/probation/styles/pds-page-templates/pds-page-templates.11tydata.js
@@ -1,8 +1,6 @@
 export default {
-  figma_link:
-    'https://www.figma.com',
-  githuburl:
-    'https://github.com',
+  figma_link: 'https://www.figma.com',
+  githuburl: 'https://github.com',
   tabCollection: 'pds-page-templates',
   tab_warning:
     'Before using this component in production, check the [readme file in the Probation Digital Services GitHub repository](https://github.com/ministryofjustice/hmpps-probation-frontend-component-api) for instructions on how to implement it.',
