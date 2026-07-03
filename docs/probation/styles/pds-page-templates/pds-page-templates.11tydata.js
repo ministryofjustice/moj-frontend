ce0276ac0668368bd745468249251a57fc24e858
RoseBrandle <rose.brandle@justice.gov.uk>
2026-07-03 14:53:20 +0100

    docs: resolving issues in 11ty file

--- docs/probation/styles/pds-page-templates/pds-page-templates.11tydata.js
+++ docs/probation/styles/pds-page-templates/pds-page-templates.11tydata.js
@@ -4,5 +4,7 @@ export default {
   githuburl:
     'https://github.com',
   tabCollection: 'pds-page-templates',
+  tab_warning:
+    'Before using this component in production, check the [readme file in the Probation Digital Services GitHub repository](https://github.com/ministryofjustice/hmpps-probation-frontend-component-api) for instructions on how to implement it.',
   blockTitle: 'Page templates'
 }
