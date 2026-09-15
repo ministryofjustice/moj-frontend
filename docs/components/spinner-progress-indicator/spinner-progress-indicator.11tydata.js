cd369c07fe1dd1ac01a8937953043c9e54bfe1a1
Chris Pymm <chris.pymm@digital.justice.gov.uk>
2026-09-15 13:29:48 +0100

    docs: add github discussion link to spinner progress indicator

--- docs/components/spinner-progress-indicator/spinner-progress-indicator.11tydata.js
+++ docs/components/spinner-progress-indicator/spinner-progress-indicator.11tydata.js
@@ -2,7 +2,7 @@ export default {
   figma_link:
     'https://www.figma.com/design/N2xqOFkyehXwcD9DxU1gEq/MOJ-Figma-Kit?m=auto',
   githuburl:
-    'https://github.com/ministryofjustice/moj-frontend/discussions/categories/experimental-components-pages-and-patterns',
+    'https://github.com/ministryofjustice/moj-frontend/discussions/2641',
   tabCollection: 'spinner-progress-indicator',
   blockTitle: 'Spinner progress indicator'
 }
